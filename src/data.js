1b28e162bc2c85a250f3589bfe138d5cfbe53375
Victoria Rain Coleman <victoriarainc@gmail.com>
2017-09-04 19:13:49 -0400

    added data arrays -need to be corrected

---
+++ src/data.js
@@ -0,0 +1,65 @@
+let cards = [
+  {
+    "id": "1",
+    "type": "thanks",
+    "price": "$3",
+    "description": "Recyled shipping paper hand crafted to create a vintage parcel look.",
+    "img": "pic goes here"
+  }, {
+    "id": "2",
+    "type": "sympathy",
+    "price": "$3",
+    "description": "Recyled shipping paper hand crafted to create a vintage parcel look.",
+    "img": "pic goes here"
+  }, {
+    "id": "3",
+    "type": "congrats",
+    "price": "$3",
+    "description": "Recyled shipping paper hand crafted to create a vintage parcel look.",
+    "img": "pic goes here"
+  }
+]
+
+let stationery = [
+  {
+    "id": "1",
+    "type": "moglea",
+    "price": "$9",
+    "description": "Recyled shipping paper hand crafted to create a vintage parcel look.",
+    "img": "pic goes here"
+  }, {
+    "id": "2",
+    "type": "honeycomb",
+    "price": "$9",
+    "description": "Recyled shipping paper hand crafted to create a vintage parcel look.",
+    "img": "pic goes here"
+  }, {
+    "id": "3",
+    "type": "parrots",
+    "price": "$9",
+    "description": "Recyled shipping paper hand crafted to create a vintage parcel look.",
+    "img": "pic goes here"
+  }
+]
+
+let singles = [
+  {
+    "id": "1",
+    "type": "4bar",
+    "price": "$2",
+    "description": "Recyled shipping paper hand crafted to create a vintage parcel look.",
+    "img": "pic goes here"
+  }, {
+    "id": "2",
+    "type": "#1",
+    "price": "$2",
+    "description": "Recyled shipping paper hand crafted to create a vintage parcel look.",
+    "img": "pic goes here"
+  }, {
+    "id": "3",
+    "type": "custom",
+    "price": "$3",
+    "description": "Recyled shipping paper hand crafted to create a vintage parcel look.",
+    "img": "pic goes here"
+  }
+]
