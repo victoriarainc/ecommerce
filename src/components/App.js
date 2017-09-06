a369750d09a5d7467b7e8c1381394f0005ca8dcf
Victoria Rain Coleman <victoriarainc@gmail.com>
2017-09-06 10:41:50 -0400

    added styling and files for a background video

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 
+import {NavLink} from 'react-router-dom';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
       <p> Welcome to Handmade by Vi! </p>
+
+      <div className="categories">
+        <div className="cat"><NavLink to="/cards">Greeting Cards</NavLink></div>
+        <div className="cat"><NavLink to="/stationery">Stationery</NavLink></div>
+        <div className="cat"><NavLink to="/singles">Single Letter Pieces</NavLink></div>
+      </div>
+
       </div>
     );
   }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -17,12 +17,6 @@ export default class NavBar extends Component {
           <div className="link"><NavLink to="/contact">Contact</NavLink></div>
         </div>
 
-      <div className="categories">
-        <div className="link"><NavLink to="/cards">Greeting Cards</NavLink></div>
-        <div className="link"><NavLink to="/stationery">Stationery</NavLink></div>
-        <div className="link"><NavLink to="/singles">Single Letter Pieces</NavLink></div>
-      </div>
-
       </div>
     );
   }
