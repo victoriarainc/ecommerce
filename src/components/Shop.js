b4d7b7caeb0404eca02d6fe3f2ba862f5684a9e8
Victoria Rain Coleman <victoriarainc@gmail.com>
2017-09-07 18:28:44 -0400

    successfully routes shop items by accessing data.js

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,40 +1,63 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import '../styles/App.css';
 
-import { data } from '../data.js';
+import {data} from '../data.js';
 
 import {NavLink} from 'react-router-dom';
 
-export default class App extends Component {
+export default class Shop extends Component {
   constructor(props) {
     super(props);
 
+    let match = this.props.match;
+    console.log(match);
   }
-  let items = data.map((item) => {
-    return(
-      <div className="itemwrapper">
-        <img src={item.img} />
-        <div className="itemdescription">
-          <div className="itemprice">{item.price}</div>
-          <div>{item.description}</div>
-        </div>
-      </div>
-    )
-  })
-  return (
-    <div>
-    {items}
-    </div>
-  );
-}
-}
+
   render() {
+
+    let categories = []
+    data.forEach((item) => {
+      if (categories.indexOf(item.category) === -1) {
+        categories.push(item.category);
+      }
+    });
+
+    let category_links = categories.map((item) => {
+      return (
+        <div key={item} className="link shop">
+          <NavLink to={`/shop/${item}`}>{item}</NavLink>
+        </div>
+      )
+    });
+    console.log(category_links);
+
+    // let items = data.map((item) => {
+    //   return (
+    //     <div className="itemwrapper">
+    //       <img src={item.img}/>
+    //       <div className="itemdescription">
+    //         <div className="itemprice">{item.price}</div>
+    //         <div>{item.description}</div>
+    //       </div>
+    //     </div>
+    //   );
+    // });
+
     return (
-      <div className="categories">
-         <div className="link shop"><NavLink to={`/shop/${cards}`}>Greeting Cards</NavLink></div>
-         <div className="link shop"><NavLink to={`/shop/${stationery}`}>Stationery</NavLink></div>
-         <div className="link shop"><NavLink to={`/shop/${singles}`}>Single Letter Pieces</NavLink></div>
-       </div>
+      <div>
+        {category_links}
+      </div>
+// {      <div className="categories">
+//         <div className="link shop">
+//           <NavLink to={`/shop/${cards}`}>Greeting Cards</NavLink>
+//         </div>
+//         <div className="link shop">
+//           <NavLink to={`/shop/${stationery}`}>Stationery</NavLink>
+//         </div>
+//         <div className="link shop">
+//           <NavLink to={`/shop/${singles}`}>Single Letter Pieces</NavLink>
+//         </div>
+//       </div>}
     );
   }
 }
--- src/components/ShowShop.js
+++ src/components/ShowShop.js
@@ -1,22 +1,38 @@
 import React, { Component } from 'react';
-import { singles, stationery, greetingcards } from '../data';
+import { data } from '../data';
 
 export default class ShowShop extends Component {
   constructor(props) {
     super(props);
   }
     render() {
-      let items = stationery.map((item) => {
-        return(
-          <div className="itemwrapper">
-            <img src={item.img} />
-            <div className="itemdescription">
-              <div className="itemprice">{item.price}</div>
-              <div>{item.description}</div>
+      // let items = stationery.map((item) => {
+      //   return(
+      //     <div className="itemwrapper">
+      //       <img src={item.img} />
+      //       <div className="itemdescription">
+      //         <div className="itemprice">{item.price}</div>
+      //         <div>{item.description}</div>
+      //       </div>
+      //     </div>
+      //   )
+      // });
+
+      let match = this.props.match;
+      let category = match.params.id;
+
+      let items = data.map((item) => {
+        if (item.category === category) {
+          return (
+            <div key={item.id}>
+              {item.id} - {item.category}
             </div>
-          </div>
-        )
+          )
+        } else {
+          return null;
+        }
       })
+
       return (
         <div>
         {items}
