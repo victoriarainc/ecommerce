e751e2143288d1681f5e6553a07791e860df487e
Victoria Rain Coleman <victoriarainc@gmail.com>
2017-09-08 12:15:08 -0400

    fixed empty div bug! styled Shop and Craft by adjusting .children

--- src/product.js
+++ src/product.js
@@ -76,29 +76,7 @@ let product = [
     "price": "$2",
     "description": "Oval. 2 1/2 inches wide. Color options vary.",
     "img": "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/21270971_1298686413573159_3509833774559368329_n.jpg?oh=ac940187ccce2727beadc5217ce93fc8&oe=5A1AFF06"
-  }, {
-    "service": 'handlettering',
-    "id": "11",
-    "type": "custom",
-    "price": "$2 per card",
-    "description": "Small letter sized comes with recycled trifold letter piece.",
-    "img": "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/21270971_1298686413573159_3509833774559368329_n.jpg?oh=ac940187ccce2727beadc5217ce93fc8&oe=5A1AFF06"
-  }, {
-    "service": 'typewriter',
-    "id": "12",
-    "type": "custom",
-    "price": "$5 per card",
-    "description": "4bar and A2 available.",
-    "img": "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/21270971_1298686413573159_3509833774559368329_n.jpg?oh=ac940187ccce2727beadc5217ce93fc8&oe=5A1AFF06"
-  }, {
-    "service": 'custom envelopes',
-    "id": "13",
-    "type": "custom",
-    "price": "prices vary",
-    "description": "4bar, A2 and A6 sizes available.",
-    "img": "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/21270971_1298686413573159_3509833774559368329_n.jpg?oh=ac940187ccce2727beadc5217ce93fc8&oe=5A1AFF06"
   }
-
 ]
 
 export {product};
