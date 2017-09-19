25e3adeaf67a0aa1015b5a8b691af18f893d087d
Victoria Rain Coleman <victoriarainc@gmail.com>
2017-09-19 11:48:25 -0400

    changed introImg to link rather than background

--- src/components/App.js
+++ src/components/App.js
@@ -7,9 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App home">
-      <p>Welcome to</p>
-      <h1>Handmade by Vi!</h1>
-      <div className="introImg"></div>
+      <img src="https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/21686041_1309955992446201_7507814998148813410_n.jpg?oh=da6959435b77f718fb44f65246a5c81b&oe=5A3B837E" className="introImg"/>
       </div>
     );
   }
