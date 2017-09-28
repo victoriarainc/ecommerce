c32b794295a98559ee55d6d8179788689c169c04
Victoria Rain Coleman <victoriarainc@gmail.com>
2017-09-27 21:32:04 -0400

    altered offer descriptions

--- src/offer.js
+++ src/offer.js
@@ -4,21 +4,21 @@ let craft = [
     "id": "111",
     "type": "custom",
     "price": "$2 per card",
-    "description": "Small letter sized comes with recycled trifold letter piece.",
+    "description": "Any letter can be improved with a handwritten touch! Pick from a variety of fonts to complete your look. Handlettering is also available on business signs, chalkboards, gift boxes, and even journal entries!",
     "img": "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/21270971_1298686413573159_3509833774559368329_n.jpg?oh=ac940187ccce2727beadc5217ce93fc8&oe=5A1AFF06"
   }, {
     "service": 'typewriter',
     "id": "112",
     "type": "custom",
     "price": "$5 per card",
-    "description": "4bar and A2 available.",
+    "description": "4bar and A2 sizes available. Only light colored, text weight paper will run through the machine. Serif and script font available.",
     "img": "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/21270971_1298686413573159_3509833774559368329_n.jpg?oh=ac940187ccce2727beadc5217ce93fc8&oe=5A1AFF06"
   }, {
     "service": 'custom envelopes',
     "id": "113",
     "type": "custom",
     "price": "prices vary",
-    "description": "4bar, A2 and A6 sizes available.",
+    "description": "4bar, A2 and A6 sizes available in either given or provided paper. Available patterns range from fine papers available through Paper Source and some Handmade by Vi exclusive designs.",
     "img": "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/21270971_1298686413573159_3509833774559368329_n.jpg?oh=ac940187ccce2727beadc5217ce93fc8&oe=5A1AFF06"
   }
 
