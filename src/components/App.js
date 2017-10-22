98786ebbbb5a5eefc1efa3a4b30aecd17a7a6539
Victoria Rain Coleman <victoriarainc@gmail.com>
2017-10-21 22:54:59 -0400

    added background image to home page

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App home">
+      <p className="homeSpan">Handmade by Vi</p>
       { // <img src="https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/21686041_1309955992446201_7507814998148813410_n.jpg?oh=da6959435b77f718fb44f65246a5c81b&oe=5A3B837E" className="introImg"/>
     }
       </div>
